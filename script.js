c6ed252d859d4cc15babd01a179f66a59f37837e
Nishant Kumar Singh <nishant220902@gmail.com>
2021-10-11 23:05:03 +0530

    added menu button and its css and js

--- script.js
+++ script.js
@@ -6,5 +6,10 @@ $(document).ready(function(){
             $('nav').removeClass("sticky");
         }
         
-    })
+    });
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('nav .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
 });
