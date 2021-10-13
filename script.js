2e753795a68963754f75b216e5d6c4cb9e89220e
Nishant Kumar Singh <nishant220902@gmail.com>
2021-10-13 22:18:21 +0530

    added services, skills, contact, footer section and its style and added scroll up button

--- script.js
+++ script.js
@@ -5,8 +5,20 @@ $(document).ready(function(){
         }else{
             $('nav').removeClass("sticky");
         }
-        
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }
+        else{
+            $('.scroll-up-btn').removeClass("show")
+        }
+
     });
+
+    // slide up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+
     // toggle menu/navbar script
     $('.menu-btn').click(function(){
         $('nav .menu').toggleClass("active");
