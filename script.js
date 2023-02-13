0d6f466f73d3da151e1fd99832797f78571c6ac5
Nishant Kumar Singh <nishant220902@gmail.com>
2023-02-13 11:05:54 +0530

    corrected indentation

--- script.js
+++ script.js
@@ -1,41 +1,39 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 20){
-            $('nav').addClass("sticky");
-        }else{
-            $('nav').removeClass("sticky");
-        }
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }
-        else{
-            $('.scroll-up-btn').removeClass("show")
-        }
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $("nav").addClass("sticky");
+    } else {
+      $("nav").removeClass("sticky");
+    }
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
 
-    });
+  // slide up script
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+  });
 
-    // slide up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-    });
+  // toggle menu/navbar script
+  $(".menu-btn").click(function () {
+    $("nav .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
 
-    // toggle menu/navbar script
-    $('.menu-btn').click(function(){
-        $('nav .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
-
-    // typing animation script
-    var typed = new Typed(".typing", {
-        strings: ["an IT Student", "a Coder", "a Frontend Developer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-    var typed = new Typed(".typing2", {
-        strings: ["an IT Student", "a Coder", "a Frontend Developer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+  // typing animation script
+  var typed = new Typed(".typing", {
+    strings: ["an IT Student", "a Coder", "a Frontend Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+  var typed = new Typed(".typing2", {
+    strings: ["an IT Student", "a Coder", "a Frontend Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
 });
